41b91ed3fbca712c66a948c52725d21c58c64c37
Andrei Radu <47434163+AndreiMVP@users.noreply.github.com>
2022-09-05 18:30:23 +0300

    subgraph

--- src/constants/subgraph.ts
+++ src/constants/subgraph.ts
@@ -3,6 +3,8 @@ import { ChainId } from "./chains";
 export const SUBGRAPH_ENDPOINTS = {
   [ChainId.RINKEBY]:
     "https://api.thegraph.com/subgraphs/name/andreimvp/poh-rinkeby",
+  [ChainId.GOERLI]:
+    "https://api.thegraph.com/subgraphs/name/andreimvp/poh-goerli",
   [ChainId.MAINNET]:
     "https://api.thegraph.com/subgraphs/name/andreimvp/poh-v1-in-v2-shoes",
   [ChainId.GNOSIS]:
