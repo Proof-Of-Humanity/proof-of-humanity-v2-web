1f0829fe16a55f0a8ae90aac4a24cf5324c500c0
madhurMongia <madhurrajput1010@gmail.com>
2024-12-31 17:26:13 +0530

    refactor: simplify policy display in request page by removing redundant elements and comments

---
+++ src/components/DocumentIcon.tsx
@@ -0,0 +1,8 @@
+"use client";
+
+import React from "react";
+import NoteMajorIcon from "icons/NoteMajor.svg";
+
+export default function DocumentIcon(props: React.SVGProps<SVGElement>) {
+  return <NoteMajorIcon {...props} />;
+} 
