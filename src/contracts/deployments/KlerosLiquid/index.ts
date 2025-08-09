95a60b8bcff5e9cbef1521152d35a48a63b7e379
madhurMongia <madhurrajput1010@gmail.com>
2025-08-09 21:53:46 +0530

    refactor: remove AirdropClientState and integrate features into ClaimSection
    - Deleted AirdropClientState component to streamline the codebase.
    - Enhanced ClaimSection with new eligibility checks and improved user feedback.
    - Introduced loading and error states for better user experience during claim processes.
    - Updated GraphQL queries to fetch processed airdrop data, consolidating logic within ClaimSection.
    - Added FeatureList component for displaying features and benefits in a structured format.
    - Removed unused icons and components to clean up the codebase.

--- src/contracts/deployments/KlerosLiquid/index.ts
+++ src/contracts/deployments/KlerosLiquid/index.ts
@@ -1,11 +1,11 @@
 import { mainnet, sepolia, gnosis, gnosisChiado } from "viem/chains";
 import abi from "./abi";
 
-const addresses = {
-  [mainnet.id]: null,
-  [sepolia.id]: null,
-  [gnosis.id]: null,
-  [gnosisChiado.id]: null,
+const addresses: Record<number, `0x${string}` | undefined> = {
+  [mainnet.id]: '0x988b3A538b618C7A603e1c11Ab82Cd16dbE28069',
+  [sepolia.id]: '0x90992fb4E15ce0C59aEFfb376460Fda4Ee19C879',
+  [gnosis.id]: '0x9C1dA9A04925bDfDedf0f6421bC7EEa8305F9002',
+  [gnosisChiado.id]: '0xD8798DfaE8194D6B4CD6e2Da6187ae4209d06f27',
 };
 
 export const KlerosLiquid = {
