9d3dc29c06b1442633606c54bae7eeb99316d631
madhurMongia <madhurrajput1010@gmail.com>
2025-08-18 23:49:15 +0530

    refactor: simplify EthereumAMBBridge by removing unused chain imports
    - Removed Gnosis and GnosisChiado imports and their corresponding addresses from EthereumAMBBridge.
    - Streamlined the code for better maintainability and clarity.

--- src/contracts/deployments/EthereumAMBBridge/index.ts
+++ src/contracts/deployments/EthereumAMBBridge/index.ts
@@ -1,11 +1,9 @@
-import { gnosis, gnosisChiado, mainnet, sepolia } from "viem/chains";
+import { mainnet, sepolia } from "viem/chains";
 import abi from "./abi";
 
 const addresses = {
   [mainnet.id]: "0x4C36d2919e407f0Cc2Ee3c993ccF8ac26d9CE64e" as const,
   [sepolia.id]: "0xf2546D6648BD2af6a008A7e7C1542BB240329E11" as const,
-  [gnosis.id]: "0x4C36d2919e407f0Cc2Ee3c993ccF8ac26d9CE64e" as const,
-  [gnosisChiado.id]: "0xf2546D6648BD2af6a008A7e7C1542BB240329E11" as const,
 };
 
 export const EthereumAMBBridge = {
