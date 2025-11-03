434f6f374861fc536b202cbf7eaec059eee175de
madhurMongia <madhurrajput1010@gmail.com>
2025-11-03 19:22:31 +0530

    feat: update PnkAirdrop and Kleros data handling for Mainnet support
    - Added support for Sepolia chain in PnkAirdrop component and updated airdropChainId logic.
    - Included Sepolia address in PnkRewardDistributer deployment records.
    - Introduced new API endpoint for Kleros on Mainnet and refactored staking functions to fetch total PNK staked across Mainnet and Gnosis.
    - Updated KIP66 configuration values for improved target metrics and adjusted related functions for consistency.

--- src/contracts/deployments/PnkRewardDistributer/index.ts
+++ src/contracts/deployments/PnkRewardDistributer/index.ts
@@ -1,7 +1,8 @@
-import { gnosis, gnosisChiado } from "viem/chains";
+import { gnosis, gnosisChiado, sepolia } from "viem/chains";
 import abi from "./abi";
 
 const addresses: Record<number, `0x${string}` | undefined> = {
+  [sepolia.id]: "0xbe372A981fCFa743C02C7ddeee728e5eD74Ea11E",
   [gnosis.id]: "0x",
   [gnosisChiado.id]: "0x3Bc210D458095f9E8c704951290a984f2fbF42EA",
 };
