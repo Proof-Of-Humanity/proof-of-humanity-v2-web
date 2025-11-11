c65fe2cf381a731a49004797dd4319935386b989
madhurMongia <madhurrajput1010@gmail.com>
2025-11-11 20:28:58 +0530

    fix: update Gnosis address in CirclesIntegration deployment
    - Set the Gnosis address in the CirclesIntegration deployment to the correct contract address for improved functionality.

--- src/contracts/deployments/CirclesIntegration/index.ts
+++ src/contracts/deployments/CirclesIntegration/index.ts
@@ -2,7 +2,7 @@ import { gnosis, gnosisChiado } from "viem/chains";
 import abi from "./abi";
 
 const addresses = {
-  [gnosis.id]: null,
+  [gnosis.id]: "0xf49aB03E980BD27ecf9352cAF4A65921DD70a554" as const,
   [gnosisChiado.id]: "0x7562C66dB28e397c81d1E6d7645B59D308dEda46" as const,
 };
 
